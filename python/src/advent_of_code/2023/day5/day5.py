cdf686a33b87ff68505e3de4ce39c526af0e5733
Isaac Owomugisha <archimedesisaac@gmail.com>
2023-12-09 21:16:32 +0300

    aoc day 5, part 2

--- python/src/advent_of_code/2023/day5/day5.py
+++ python/src/advent_of_code/2023/day5/day5.py
@@ -25,52 +25,105 @@ def read_ints():
 
 
 @dataclasses.dataclass
-class Range:
+class Mapping:
     destination_start: int
     source_start: int
     length: int
 
+@dataclasses.dataclass
+class Range:
+    """[start, end) ranges, where start is inclusive and end is exclusive"""
+    start: int
+    end: int
 
-def find_next(source_object: int, ranges: List[Range]) -> int:
-    for range_ in ranges:
-        if range_.source_start <= source_object < range_.source_start + range_.length:
-            return range_.destination_start + (source_object - range_.source_start)
+
+def find_next_single(source_object: int, mappings: List[Mapping]) -> int:
+    for mapping in mappings:
+        if mapping.source_start <= source_object < mapping.source_start + mapping.length:
+            return mapping.destination_start + (source_object - mapping.source_start)
     return source_object
 
 
-def find_location(seed: int, pipeline: Dict[str, List[Range]]) -> int:
-    soil = find_next(seed, pipeline["seed-to-soil"])
-    fertilizer = find_next(soil, pipeline["soil-to-fertilizer"])
-    water = find_next(fertilizer, pipeline["fertilizer-to-water"])
-    light = find_next(water, pipeline["water-to-light"])
-    temperature = find_next(light, pipeline["light-to-temperature"])
-    humidity = find_next(temperature, pipeline["temperature-to-humidity"])
-    location = find_next(humidity, pipeline["humidity-to-location"])
+def find_next_ranges(source_ranges: List[Range], mappings: List[Mapping], debug = False) -> List[Range]:
+    new_ranges = []
+    for mapping in mappings:
+        non_intersecting_ranges = []
+        for range_ in source_ranges:
+            start, end = range_.start, range_.end
+            map_start, map_end = mapping.source_start, mapping.source_start + mapping.length
+
+            before_start, before_end = start, min(end, map_start)
+            if before_start < before_end:
+                non_intersecting_ranges.append(Range(before_start, before_end))
+
+            intersection_start, intersection_end = max(start, map_start), min(end, map_end)
+            if debug:
+                print(f"{mapping}, {range_}->", intersection_start, intersection_end)
+            if intersection_start < intersection_end:
+                new_start = mapping.destination_start + (intersection_start - map_start)
+                new_end = mapping.destination_start + (intersection_end - map_start)
+                new_ranges.append(Range(new_start, new_end))
+
+            after_start, after_end = max(start, map_end), end
+            if after_start < after_end:
+                non_intersecting_ranges.append(Range(after_start, after_end))
+            if debug:
+                print(f"{mapping} -> {new_ranges}")
+        source_ranges = non_intersecting_ranges
+
+    new_ranges += source_ranges
+    # print(f"{mappings} -> {new_ranges}")
+    return new_ranges
+
+
+def find_single_location(seed: int, pipeline: Dict[str, List[Mapping]]) -> int:
+    soil = find_next_single(seed, pipeline["seed-to-soil"])
+    fertilizer = find_next_single(soil, pipeline["soil-to-fertilizer"])
+    water = find_next_single(fertilizer, pipeline["fertilizer-to-water"])
+    light = find_next_single(water, pipeline["water-to-light"])
+    temperature = find_next_single(light, pipeline["light-to-temperature"])
+    humidity = find_next_single(temperature, pipeline["temperature-to-humidity"])
+    location = find_next_single(humidity, pipeline["humidity-to-location"])
     return location
+
+
+def find_range_location(ranges: List[Range], pipeline: Dict[str, List[Mapping]]) -> int:
+    for stage in pipeline.keys():
+        # print(ranges)
+        mappings = pipeline[stage]
+        # debug = True if stage == "seed-to-soil" else False
+        ranges = find_next_ranges(ranges, mappings)
+    ranges.sort(key=lambda range_: range_.start)
+    return ranges[0].start
 
 
 def solve():
     pipeline = defaultdict(list)
     _, seeds_str = read_line().split(": ")
     seeds = map(int, seeds_str.strip().split())
+    ranges = []
+    seeds_list = list(seeds)
+    for i in range(0, len(seeds_list), 2):
+        ranges.append(Range(seeds_list[i], seeds_list[i] + seeds_list[i + 1]))
 
     read_line()
     while True:
         map_type, _ = read_line().split()
         range_str = read_line()
-        ranges = []
+        mappings = []
         while range_str:
             dest_start, src_start, length = map(int, range_str.split())
-            ranges.append(Range(dest_start, src_start, length))
+            mappings.append(Mapping(dest_start, src_start, length))
             range_str = read_line()
 
-        pipeline[map_type] = ranges
+        pipeline[map_type] = mappings
         if map_type == "humidity-to-location":
             break
 
-    locations = list(map(lambda seed: find_location(seed, pipeline), seeds))
-    print(locations)
+    locations = list(map(lambda seed: find_single_location(seed, pipeline), seeds_list))
+    # print(locations)
     print(f"Part 1: {min(locations)}")
+    print(f"Part 2: {find_range_location(ranges, pipeline)}")
 
 
 if __name__ == '__main__':
