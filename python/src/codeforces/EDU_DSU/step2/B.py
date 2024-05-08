d8ed0dcb9c2bb34b6572f737b468c749818e0328
Isaac Owomugisha <archimedesisaac@gmail.com>
2024-05-08 18:58:51 +0300

    solve EDU DSU step 2 B

--- python/src/codeforces/EDU_DSU/step2/B.py
+++ python/src/codeforces/EDU_DSU/step2/B.py
@@ -35,22 +35,46 @@ def get_parent(slot, parent):
     return slot
 
 
+class DSU:
+
+    def __init__(self, size: int):
+        self.parents = [i for i in range(size + 1)]
+        self.size = [1 for _ in range(size + 1)]
+
+    def get_parent(self, node: int):
+        path = []
+        while node != self.parents[node]:
+            path.append(node)
+            node = self.parents[node]
+
+        parent = node
+        for node in path:
+            self.parents[node] = parent
+        return parent
+
+    def union(self, first: int, second: int):
+        first, second = self.get_parent(first), self.get_parent(second)
+        if first == second:
+            return
+
+        self.parents[first] = second
+        self.size[second] += self.size[first]
+
+    def get_size(self, node: int):
+        return self.size[self.get_parent(node)]
+
+
 def solve():
     n = read_int()
 
     p = read_ints()
-
-    parent = [i for i in range(n + 1)]
-
-    ans = []
-    for i in range(n):
-        next_free_slot = get_parent(p[i], parent)
-        ans.append(next_free_slot)
-        parent[next_free_slot] = (next_free_slot + 1) % (n + 1)
-        if parent[next_free_slot] == 0:
-            parent[next_free_slot] = 1
-
-    print_(f"{' '.join(map(str, ans))}\n")
+    dsu_parking = DSU(n)
+    actual_parking = []
+    for i, parking_slot in enumerate(p):
+        free_slot = dsu_parking.get_parent(parking_slot)
+        actual_parking.append(free_slot)
+        dsu_parking.union(free_slot, free_slot % n + 1)
+    print_(f"{' '.join(map(str, actual_parking))}\n")
 
 
 if __name__ == '__main__':
