d0c2e21013b2bb6125a5f7ab996f40ea403e0992
Isaac Owomugisha <archimedesisaac@gmail.com>
2024-08-27 22:07:47 +0300

    failed to solve CF966 F, try again later

--- python/src/codeforces/CF966/F.py
+++ python/src/codeforces/CF966/F.py
@@ -22,8 +22,50 @@ def read_ints():
     return list(map(int, read_line().split()))
 
 
+def get_minimum_operations(k, a, b):
+    ops = 0
+    for _ in range(k):
+        if a < b:
+            ops += a
+            b -= 1
+        else:
+            ops += b
+            a -= 1
+    return ops
+
+
 def solve():
-    pass
+    INF = int(1e18)
+    t = read_int()
+    for _ in range(t):
+        n, K = read_ints()
+        rectangles = []
+        for _ in range(n):
+            a, b = read_ints()
+            if a > b:
+                a, b = b, a
+            rectangles.append((a, b))
+        dp = [[INF for _ in range(K + 1)] for _ in range(n)]
+        ans = INF
+        for i in range(n):
+            for k in range(K + 1):
+                a, b = rectangles[i]
+                if i == 0:
+                    dp[i][k] = INF if k > a + b else get_minimum_operations(k, a, b)
+                else:
+                    if k <= a + b:
+                        take = get_minimum_operations(k, a, b)
+                    else:
+                        take = INF
+                    if dp[i - 1][k - min(k, a + b)] == INF or take == INF:
+                        take = INF
+                    else:
+                        take += dp[i - 1][k - min(k, a + b)]
+                    dp[i][k] = min(take, dp[i - 1][k])
+                if k == K:
+                    ans = min(dp[i][k], ans)
+        ans = -1 if ans == INF else ans
+        print_(f"{ans}\n")
 
 
 if __name__ == '__main__':
